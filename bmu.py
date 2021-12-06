ca71da7667602cc62b9bff82a1fff4f879520001
AUTOxSTAR <95621176+AUTOxSTAR@users.noreply.github.com>
2021-12-06 20:02:00 +0800

    Update bmu.py

--- bmu.py
+++ bmu.py
@@ -6,16 +6,16 @@ def BMI(w,h):
 #w = float(input('請輸入體重(KG)?'))
 w = st.number_input('請輸入體重(KG)？')
 h = st.number_input('請輸入身高(M)？')
-st.button('請輸入確認')
-
-bmi = BMI(w, h)
-#print('BMI為', bmi)
-st.write('BMI為')
-if (bmi < 18):
-    print('體重過輕')
-elif (bmi < 24):
-    print('體重正常')
-elif (bmi < 27):
-    print('體重過重')
-else:
-    print('體重肥胖')
+confirm_input = st.button('請輸入確認')
+if confirm_input:
+    bmi = BMI(w, h)
+    #print('BMI為', bmi)
+    st.write('BMI為')
+    if (bmi < 18):
+        print('體重過輕')
+    elif (bmi < 24):
+        print('體重正常')
+    elif (bmi < 27):
+        print('體重過重')
+    else:
+        print('體重肥胖')
