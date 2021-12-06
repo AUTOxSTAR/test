63456a9b46c040f80349cf1604eecd1f1b7b6491
AUTOxSTAR <95621176+AUTOxSTAR@users.noreply.github.com>
2021-12-06 20:51:07 +0800

    Update gas.py

--- gas.py
+++ gas.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
 gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
-speed= st.number_input('車速的資料收集:限速100 =>')
-temp = st.number_input('溫度的資料收集:限溫30 =>')
-轉速=st.number_input('轉速資料的收集:限轉12000 =>')
+speed= st.number_input('車速的資料收集:限速70 =>')
+temp = st.number_input('溫度的資料收集:限溫200 =>')
+轉速=st.number_input('轉速資料的收集:限轉10000 =>')
 confirm_input = st.button('請輸入確認')
 
 if gas <= car_simulation.get('gas_warning'):
